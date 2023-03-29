7374e65ac760ccad017f9f631d38ffc79b9e649e
Nick <nickvanbokhorst@hotmail.com>
2023-03-29 15:40:44 +0200

    soong: Whitelist llvm-addr2line
    Change-Id: Ieec3d55f7b3e4041b4435c7b085d0ee106843d9c
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -110,6 +110,7 @@ var Configuration = map[string]PathConfig{
 	"lsof":           Allowed,
 	"ld.lld":  	      Allowed,
 	"llvm-ar": 	      Allowed,
+	"llvm-addr2line": Allowed,
 	"nproc":          Allowed,
 	"openssl":        Allowed,
 	"pahole":         Allowed,
