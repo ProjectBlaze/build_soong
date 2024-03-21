6c92a2ed2f9f1608df8e606408144794944a71ef
dhruvesh16 <dhruvesh3466@protonmail.com>
2024-03-21 20:58:13 +0530

    Revert "soong: add support for build data-binding apps"
    This reverts commit 3aa1075b405d3c1261aa6760877dc1fb7488a7e7.

--- java/config/config.go
+++ java/config/config.go
@@ -1,5 +1,4 @@
 // Copyright 2017 Google Inc. All rights reserved.
-// Copyright 2022 Project Kaleidoscope. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -37,8 +36,6 @@ var (
 	FrameworkLibraries                       = []string{"ext", "framework"}
 	DefaultLambdaStubsLibrary                = "core-lambda-stubs"
 	SdkLambdaStubsPath                       = "prebuilts/sdk/tools/core-lambda-stubs.jar"
-	DataBindingDepClassInfoPath              = "prebuilts/derp/libraries/current/data-binding/class-info"
-	DataBindingDepArtifactsPath              = "prebuilts/derp/libraries/current/data-binding/artifacts"
 
 	DefaultMakeJacocoExcludeFilter = []string{"org.junit.*", "org.jacoco.*", "org.mockito.*"}
 	DefaultJacocoExcludeFilter     = []string{"org.junit.**", "org.jacoco.**", "org.mockito.**"}
@@ -215,11 +212,6 @@ func init() {
 	// TODO(ccross): this should come from the signapk dependencies, but we don't have any way
 	// to express host JNI dependencies yet.
 	hostJNIToolVariableWithSdkToolsPrebuilt("SignapkJniLibrary", "libconscrypt_openjdk_jni")
-
-	pctx.HostBinToolVariable("DataBinderCmd", "data-binder")
-	pctx.HostJavaToolVariable("DataBinderJar", "data-binder.jar")
-	pctx.SourcePathVariable("DataBindingDepClassInfoPath", DataBindingDepClassInfoPath)
-	pctx.SourcePathVariable("DataBindingDepArtifactsPath", DataBindingDepArtifactsPath)
 }
 
 func BazelJavaToolchainVars(config android.Config) string {
